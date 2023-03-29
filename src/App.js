60cc6ecfbbcf81fb4daa0568fd89937c13031cce
Narmadha1401 <narmadha1416@gmail.com>
2023-03-29 14:34:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+App.js
+import logo from './logo.svg';
+import './App.css';
+import FruitSurvey from './Comp/Fruit';
+
+function App() {
+  return (
+    <div className="App">
+        <FruitSurvey/>
+    </div>
+  );
+}
+
+export default App;
